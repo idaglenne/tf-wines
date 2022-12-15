82951df390f33ea736aef957e9727996e57bc6a2
Ida G <idaglenne@gmail.com>
2022-12-15 20:52:58 +0100

    order wines by product launch date

--- src/services/wine.service.tsx
+++ src/services/wine.service.tsx
@@ -1,9 +1,17 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app'
+import { initializeApp } from 'firebase/app';
 import {
-  child, get,
-  getDatabase, onValue, ref, remove, set
-} from 'firebase/database'
+  child,
+  get,
+  getDatabase,
+  onValue,
+  orderByChild,
+  query,
+  ref,
+  remove,
+  set
+} from 'firebase/database';
+import { Product } from '../models/product.model';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -12,37 +20,56 @@ import {
 const firebaseConfig = {
   apiKey: 'AIzaSyDKmNGfi1cy4g1MKaIUP5YqP9hzs1CE4NI',
   authDomain: 'tf-wines.firebaseapp.com',
-  databaseURL:
-    'https://tf-wines-default-rtdb.europe-west1.firebasedatabase.app',
+  databaseURL: 'https://tf-wines-default-rtdb.europe-west1.firebasedatabase.app',
   projectId: 'tf-wines',
   storageBucket: 'tf-wines.appspot.com',
   messagingSenderId: '418338342943',
   appId: '1:418338342943:web:4db66d568862149699ed2a',
   measurementId: 'G-3PRF7QNXZ4'
-}
+};
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig)
+const app = initializeApp(firebaseConfig);
 // const analytics = getAnalytics(app);
 
-const database = getDatabase(app)
-const dbRef = ref(database, '/')
+const database = getDatabase(app);
+const dbRef = ref(database, '/');
 
 export const writeToWines = (): void => {
-  onValue(child(dbRef, '/products'),
-    async snapshot => {
-      const wines = snapshot.val().filter((p: any) => p.categoryLevel1 === 'Vin' && p.assortmentText === 'Tillfälligt sortiment')
+  onValue(
+    child(dbRef, '/products'),
+    async (snapshot) => {
+      const wines = snapshot
+        .val()
+        .filter(
+          (p: Product) => p.categoryLevel1 === 'Vin' && p.assortmentText === 'Tillfälligt sortiment'
+        )
+        .map((w: Product) => ({
+          ...w,
+          productLaunchDate: new Date(w.productLaunchDate).getTime()
+        }));
 
-      await remove(ref(database, 'wines/'))
-      await set(ref(database, 'wines/ts/'), wines)
+      console.log(wines[0].productLaunchDate);
 
-      console.log('product name', wines)
+      await remove(ref(database, 'wines/ts/'));
+      await set(ref(database, 'wines/ts/'), wines);
+
+      // console.log('product name', wines);
     },
     {
       onlyOnce: true
-    })
-}
+    }
+  );
+};
 
-export const getWines = async (): Promise<any> => {
-  return await get(child(dbRef, '/wines/ts')).then(snapshot => snapshot.val())
-}
+export const getWines = async (): Promise<Product[]> => {
+  return await get(query(child(dbRef, '/wines/ts'), orderByChild('productLaunchDate'))).then(
+    (snapshot) => {
+      const response: Product[] = [];
+      snapshot.forEach((child) => {
+        response.unshift(child.val());
+      });
+      return response;
+    }
+  );
+};
